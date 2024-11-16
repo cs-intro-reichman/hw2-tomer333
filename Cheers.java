71965c0f699e497ce62bac9c597a50040000e12e
tomer333 <tomer.dahan71@gmail.com>
2024-11-16 15:33:23 +0200

    First HW2 Check

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,16 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String cheer = args[0].toUpperCase();
+                int times = Integer.parseInt(args[1]);
+                String cheer2 = cheer;
+                String inputCheck = "AEFHILMNORSX";
+                while (cheer.length() > 0) {
+                        if (inputCheck.indexOf(cheer.charAt(0)) != -1) {System.out.println("Give me an "+cheer.charAt(0)+": "+cheer.charAt(0)+"!");}
+                        else{System.out.println("Give me a  "+cheer.charAt(0)+": "+cheer.charAt(0)+"!");}
+                        cheer = cheer.substring(1);
+                }
+                System.out.println("What does that spell?");
+                while (times > 0) { System.out.println(cheer2+"!!!"); times--;}
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,15 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        int totalMinutes = (hours*60) + minutes + minutesToAdd;
+        int newHours = (totalMinutes/60)%24;
+        int newMinutes = totalMinutes-(totalMinutes/60)*60;
+		String Sminutes;
+		if (newMinutes < 10) {Sminutes = "0"+newMinutes;}
+		else{Sminutes = ""+newMinutes;}
+		if (newHours < 10) {System.out.print("0"+newHours+":"+Sminutes);}
+		else {System.out.print(newHours+":"+Sminutes);}
     }
 }
